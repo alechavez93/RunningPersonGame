2aa4863403db45d4405ffa347d15192c66fa6fc6
alechavez93 <alechavez93@gmail.com>
2016-01-21 02:31:34 -0500

    Game already finished and ready to play, need to implement keyboard input and search for bugs

--- script.js
+++ script.js
@@ -9,17 +9,18 @@ var background = document.getElementById("background");
 //Main here!-------------------------
 
 //Objects
-var distance = 125;
-var change = 60;
+var score = 0;
+var distance = 135;
+var change = 80;
 var deltaX = 0;
 var deltaB = 0;
 var globalTime = 0;
-var globalSpeed = 7;
+var globalSpeed = 4;
 var p = new person(20, 120);
 var o1 = new obstacle(2 , 1); //Single
 var o2 = new obstacle(1 , 1); //Single small
 var o3 = new obstacle(2 , 2); //Double
-var b = new bird(globalSpeed + 2);
+var b = new bird();
 
 
 //EventListeners
@@ -29,32 +30,45 @@ jump.addEventListener("click", initializeJump);
 start.addEventListener("click", initializeGame);
 
 
-var s = setInterval(game, 30);
-// function test(){
-// 	gameRunning = false;
-// 	alert(deltaB);
-// }
-
+var s = setInterval(game, 15);
 
 
 
 //Main functions---------------------------------------------------------
+//Initializes the jump sequence
 function initializeJump(){
-	if(p.running){
+	if(p.running && gameRunning){
 		p.running = false;
 		p.jumping = true;
 	}
 }
 
+//Starts game
 function initializeGame(){
 	gameRunning = true;
+	globalSpeed = 0;
+	score = 0;
+	distance = 135;
+	change = 80;
+	deltaX = 0;
+	deltaB = 0;
+	globalTime = 0;
+	globalSpeed = 4;
+	p = new person(20, 120);
+	o1 = new obstacle(2 , 1); //Single
+	o2 = new obstacle(1 , 1); //Single small
+	o3 = new obstacle(2 , 2); //Double
+	b = new bird();
 }
 
+//Erases canvas for new Frame
 function clearCanvas(){
 	ctx.clearRect(0,0, canvas.width, canvas.height);
 }
 
+//Update all elements in the canvas
 function updateCanvas(){
+	UPdifficulty();
 	if(p.jumping){
 		p.updateJumpY();
 	}
@@ -70,34 +84,52 @@ function updateCanvas(){
 	if(b.x > -200){
 		b.updateX();
 	}
-
 }
 
+function UPdifficulty(){
+	if(0 <= score && score < 1250){
+		globalSpeed = 4;
+	}
+	else if(1250 <= score && score < 3000){
+		globalSpeed = 5;
+	}
+	else if(3000 <= score && score < 4500){
+		globalSpeed = 6;
+	}
+	else if(4500 <= score){
+		globalSpeed = 7;
+	}
+}
+
+//
 function drawCanvas(){
 	clearCanvas();
 	drawBackground();
 	drawGround();
 	p.drawE();
 	drawObstacles();
-	globalTime += 30;
+	globalTime += 15;
 }
 
-function checkCollision(obs){
-	var error = 10;
-	if(/*Horizontal collision*/(p.x <= obs.x + obs.width && p.x + p.width >= obs.x)
-		&& /*Vertical collision*/(p.y <= obs.y + obs.height && p.y + p.height >= obs.y)){
-		gameRunning = false;
-	}
-}
 
 function game(){
 	if(gameRunning){
 		updateCanvas();
 		drawCanvas();
-		checkCollision(o);
+		drawScore();
+		checkCollision(o1);
+		checkCollision(o2);
+		checkCollision(o3);
+		checkCollision(b);
 	}
 }
 
+
+function drawScore(){
+	ctx.font="12px Comic Sans MS";
+	ctx.fillText("Score: "+score,525,20);
+	score = globalTime/15;
+}
 
 function drawGround(){
 	if(deltaX >= 595)
@@ -113,6 +145,7 @@ function drawBackground(){
 	ctx.drawImage(background, deltaB, 0, 1200, 400, 0, 0, 600, 200);
 	deltaB += 1;
 }
+
 
 var kind;
 function drawObstacles(){
@@ -171,7 +204,7 @@ function checkDistance(inserted,object){
 	if(Object.is(object, b) && (600 - object.x > (distance + change))){
 		return true;
 	}
-	if(Object.is(object, o2) && !(Object.is(object, b)) && (600 - object.x > (distance - change + 20))){
+	if(Object.is(object, o2) && !(Object.is(inserted, b)) && (600 - object.x > (distance - 10))){
 		return true;
 	}
 	else if(600 - object.x > (distance + change)){
@@ -181,8 +214,80 @@ function checkDistance(inserted,object){
 }
 
 
+var x,y,height,width;
+function checkCollision(obs){
+	if(p.currFrame == 0 || p.currFrame == 1 || p.currFrame == 6){
+		x = p.x; width = p.width - 4; y = p.y + 10; height = p.height;
+		return checkSingleCollision(x,y,height,width,obs);
+	}
+	//JUMPING DOWN FRAME
+	else if(p.currFrame == 2){
+		if(p.running){
+			x = p.x; width = p.width - 4; y = p.y; height = p.height;
+			return checkSingleCollision(x,y,height,width,obs);
+		}
+		else{
+			if(Object.is(b, obs)){
+				x = p.x; width = p.width - 4; y = p.y + 10; height = p.height;
+				return checkSingleCollision(x,y,height,width,obs);
+			}
+			else{
+				x = p.x + 14; width = p.width - 17; y = p.y; height = p.height - 2;
+				return checkSingleCollision(x,y,height,width,obs);
+			}
+		}
+	}
+	
+	else if(p.currFrame == 3){
+		x = p.x; width = p.width - 4; y = p.y + 10; height = p.height;
+		return checkSingleCollision(x,y,height,width,obs);
+	}
+	//JUMPING UP FRAME
+	else if(p.currFrame == 4){
+		if(p.running){
+			x = p.x; width = p.width - 4; y = p.y; height = p.height;
+			return checkSingleCollision(x,y,height,width,obs);
+		}
+		else{
+			if(Object.is(b, obs)){
+				x = p.x; width = p.width - 4; y = p.y + 10; height = p.height + 20;
+				return checkSingleCollision(x,y,height,width,obs);
+			}
+			else{
+				if(p.y < 65){
+					return true;
+				}
+				else{
+					x = p.x; width = p.width - 14; y = p.y; height = p.height;
+					return checkSingleCollision(x,y,height,width,obs);
+				}
+			}
+		}
+	}
+	else if(p.currFrame == 5){
+		x = p.x; width = p.width - 4; y = p.y + 10; height = p.height;
+		return checkSingleCollision(x,y,height,width,obs);
+	}
+}
 
 
+function checkSingleCollision(x,y,height,width,obs){
+	if(Object.is(obs, b)){
+		if(/*Horizontal collision*/(x <= (obs.x + 15) + obs.width && x + width >= (obs.x + 15))
+		&& /*Vertical collision*/(y <= (obs.y - 3) + obs.height && y + height >= (obs.y-3))){
+			gameRunning = false;
+		}
+		else return true;
+	}
+	else{
+		if(/*Horizontal collision*/(x <= obs.x + obs.width && x + width >= obs.x)
+		&& /*Vertical collision*/(y <= obs.y + obs.height && y + height >= obs.y)){
+		gameRunning = false;
+	}
+	else return true;
+	}
+	
+}
 
 
 
@@ -199,51 +304,60 @@ function person(x,y){
 	this.jumping = false;
 	this.currFrame = 0;
 	this.element = document.getElementsByClassName("runningAnimation");
-	this.velocity = 20;
-	this.gravity = 1.3;
+	this.velocity = 25;
+	this.gravity = 1.4;
 	this.initialVelocity = this.velocity;
 
 
 	//Updates the time from last time the function ran
 	this.updateT = function(){
-		this.t += 30; 		 
+		if(this.running){
+			this.gravity = 1.4+((globalSpeed-4)*0.05);
+			this.velocity = 25 +((globalSpeed-4)*2);
+		}
+		this.t += 15; 		 
 	}
 
 	
 	//Public jump function
-	var wayUp =  true;
+	var wayUp = true;
+	var first = true;
 	this.updateJumpY = function(){
-		//The person is jumping up
-		if(this.jumping && (wayUp)){
-			//If we get to the top update status
-			if(this.velocity < 1){		
-				wayUp = false;
-			}
-			else{
-				//Decrease y position
-				this.y -= this.velocity;
-				this.velocity /= this.gravity;	
-			}
-				
-		}//The person is on the way down
-		else if(this.jumping && !wayUp){
-			//Initial case velocity is less than 1
-			if(this.velocity < 1 && y <= this.ground)
-				this.velocity = 1;
+		if(this.t%30 == 0 || first){
+			first =  false;
+			//The person is jumping up
+			if(this.jumping && (wayUp)){
+				//If we get to the top update status
+				if(this.velocity < 1){		
+					wayUp = false;
+				}
+				else{
+					//Decrease y position
+					this.y -= this.velocity;
+					this.velocity /= this.gravity;	
+				}
+					
+			}//The person is on the way down
+			else if(this.jumping && !wayUp){
+				//Initial case velocity is less than 1
+				if(this.velocity < 1 && y <= this.ground)
+					this.velocity = 1;
 
-			//Normal way down motion
-			this.y += this.velocity;
-			this.velocity *= this.gravity;
-			//Checking for the ending of the jump
-			if(this.y > this.ground){	
-				//RESET EVERYTHING
-				this.jumping = false;
-				this.running = true;
-				this.velocity = this.initialVelocity;
-				wayUp = true;
-				this.y = this.ground;
-			}	
-		}
+				//Normal way down motion
+				this.y += this.velocity;
+				this.velocity *= this.gravity;
+				//Checking for the ending of the jump
+				if(this.y > this.ground){	
+					//RESET EVERYTHING
+					this.jumping = false;
+					this.running = true;
+					this.velocity = this.initialVelocity;
+					wayUp = true;
+					this.y = this.ground;
+					first = true;
+				}	
+			}
+	}
 	}
 
 	//Updates the frame number
@@ -264,6 +378,9 @@ function person(x,y){
 
 	//Function to draw the elemet
 	this.drawE = function(){
+		if(globalSpeed > 5 && this.currFrame == 5){
+			this.currFrame = 0;
+		}
 		ctx.drawImage(this.element[this.currFrame], this.x, this.y, this.width, this.height);
 		this.updateFrame();
 		this.updateT();
@@ -280,7 +397,6 @@ function obstacle(h , n){
 	this.x = -200;
 	this.y = 110;
 	this.element = document.getElementsByClassName("obstacle");
-	this.speed = globalSpeed;
 	this.index = h-1;
 
 	if(h == 1){
@@ -290,12 +406,9 @@ function obstacle(h , n){
 		this.index = 2;
 	}
 
-	//Copy gets/sets if necessary
-	//...........
-
 	//Updates coordinates
 	this.updateX = function(){
-		this.x -= this.speed;
+		this.x -= globalSpeed;
 	}
 
 	//Draws the obstacle
@@ -306,24 +419,23 @@ function obstacle(h , n){
 
 
 //Class for bird---------------------------------------------------------------
-function bird(speed){
-	this.height = 51;
-	this.width = 80;
+function bird(){
+	this.height = 47;
+	this.width = 50;
 	this.x = -200;
 	this.y = 55;
 	this.t = 0;
-	this.speed =  speed;
 	this.currFrame = 0;
 	this.element = document.getElementsByClassName("birdAnimation");
 
 	//Updates the coordinates
 	this.updateX = function(){
-		this.x -= this.speed;
+		this.x -= globalSpeed + 1;
 	}
 
 	//Update time
 	this.updateT = function(){
-		this.t += 30; 
+		this.t += 15; 
 	}
 
 	//Update frame
